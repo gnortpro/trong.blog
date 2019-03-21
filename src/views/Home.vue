f42cea80b630017d67c019670c74c2d0a84dfdd9
Trong <trongnguyen@x3english.com>
2019-03-21 14:56:04 +0700

    delete something

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,25 @@
 <template>
   <div id="home">
-    <articles>
+    <!-- <articles>
       <banner />
-    </articles>
+    </articles>-->
+    <about/>
 
-    <about />
-
-    <subscribe />
-
-    <social />
+    <!-- <subscribe /> -->
+    <!-- <social/> -->
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Home',
+export default {
+  name: "Home",
 
-    components: {
-      About: () => import('@/components/home/About'),
-      Articles: () => import('@/components/home/Articles'),
-      Banner: () => import('@/components/home/Banner'),
-      Social: () => import('@/components/home/Social'),
-      Subscribe: () => import('@/components/home/Subscribe')
-    }
+  components: {
+    About: () => import("@/components/home/About"),
+    // Articles: () => import('@/components/home/Articles'),
+    //Banner: () => import('@/components/home/Banner'),
+    Social: () => import("@/components/home/Social")
+    //Subscribe: () => import('@/components/home/Subscribe')
   }
+};
 </script>
